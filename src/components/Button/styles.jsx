077fea1a9df1fcf22bea491105da8155f951d5f0
Leticiamatos22 <cristinadematosleticia@gmail.com>
2023-08-03 16:21:11 -0300

    calculadora-react

---
+++ src/components/Button/styles.jsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 2px solid black ;
+    background-color:#6b7175;
+    color: white;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
